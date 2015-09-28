dbd7aae8a8ea2108547aaaeabbb1f921aff8df45
Khalid Sookia <khalidsookia@gmail.com>
2015-09-28 13:33:26 +0200

    one_way_data_binding

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function showContent1() {
+    document.getElementById('contenu1').style.display = 'block';
+    document.getElementById('contenu2').style.display = 'none';
+}
+
+function showContent2() {
+    document.getElementById('contenu1').style.display = 'none';
+    document.getElementById('contenu2').style.display = 'block';
+}
