b1470cd66b84c07dc3ac8c0274cbdbd6d33bd9f1
Khalid Sookia <khalidsookia@gmail.com>
2015-09-28 13:37:02 +0200

    filters

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function showContent1() {
-    document.getElementById('contenu1').style.display = 'block';
-    document.getElementById('contenu2').style.display = 'none';
-}
-
-function showContent2() {
-    document.getElementById('contenu1').style.display = 'none';
-    document.getElementById('contenu2').style.display = 'block';
-}
